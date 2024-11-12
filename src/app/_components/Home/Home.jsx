a8ca3af0c09a2525a47530d810e21d1817ffb10d
Mohammed Hassan1999 <120308228+MohammedHussein1999@users.noreply.github.com>
2024-11-12 11:38:48 +0200

    رسالة التعديل أو وصف التغييرات

--- src/app/_components/Home/Home.jsx
+++ src/app/_components/Home/Home.jsx
@@ -5,12 +5,23 @@ import CardBackg from "../cardBackg.jsx/Card";
 export default function Home() {
   return (
     <div>
-    <div className="cards-contaner">
-
-      <CardBackg title="Tatle1" data="A1" des='Mohamed , count , class , cotsh' />
-      <CardBackg title="Tatle2" data="A2" des='Mohamed , count , class , cotsh'  className='card-mid'/>
-      <CardBackg title="Tatle3" data="A3" des='Mohamed , count , class , cotsh' />
-    </div>
+      <div className="cards-contaner">
+        <CardBackg
+          title="Tatle1"
+          data="A1"
+          des="Mohamed , count , class , cotsh"
+        />
+        <CardBackg
+          title="Tatle2"
+          data="A2"
+          des="Mohamed , count , class , cotsh"
+        />
+        <CardBackg
+          title="Tatle3"
+          data="A3"
+          des="Mohamed , count , class , cotsh"
+        />
+      </div>
     </div>
   );
 }
